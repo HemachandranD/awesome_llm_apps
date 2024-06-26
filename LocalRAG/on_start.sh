5760945078f86081829d452a8b95a7de8fc6aa4d
Hemachandran <hema18deena@gmail.com>
2024-06-26 08:29:52 +0000

    uploaded the docs used for test and on_start script

--- LocalRAG/on_start.sh
+++ LocalRAG/on_start.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-# Run this sript to start redis & qdrant locally
+#Run this on linux machine to run the ollama
+mkdir bin
+sudo curl -L https://ollama.com/download/ollama-linux-amd64 -o /bin/ollama
+sudo chmod +x /bin/ollama
+./bin/ollama pull llama3
+mkdir -p ~/log
+~/bin/ollama serve > ~/log/ollama.log 2> ~/log/ollama.err &
 
+# Run this to start redis & qdrant locally
 docker container run --detach --publish 6379:6379 redis/redis-stack:latest
 docker container run --detach --publish 6333:6333 -v /tmp/local_qdrant:/qdrant/storage qdrant/qdrant:latest
